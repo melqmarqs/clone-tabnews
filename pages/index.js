4d86e8859628bcfd6f2a5114208b418a3538a067
Melquisedeque <melquisedequebass@gmail.com>
2026-08-19 17:53:29 -0300

    adiciona o `prettier` como dev-dep e os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Oi amor, eu te amo! Pra provar que vc me ama tbm, dê um mortal pra trás 😁</h1>
+  return (
+    <h1>
+      Oi amor, eu te amo! Pra provar que vc me ama tbm, dê um mortal pra trás 😁
+    </h1>
+  );
 }
 
 export default Home;
